99647bfd31f8faa625cd3200ce831cf9d093b942
Ronlin1 <ronlinx6@gmail.com>
2021-09-08 18:30:52 +0300

    changes made update

--- config/config/settings.py
+++ config/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
+import os
 
 from django.urls.conf import path
 
@@ -139,6 +140,7 @@ LOGOUT_REDIRECT_URL = 'photo:list'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
